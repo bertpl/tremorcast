b065189853312fe4099190bf82ace44e42cf5978
Bert Pluymers <bert.pluymers@gmail.com>
2022-05-29 11:48:55 +0200

    Make layer width configurable.

--- src/base/forecasting/models/ts_model_n_step_ahead_nn_mlp.py
+++ src/base/forecasting/models/ts_model_n_step_ahead_nn_mlp.py
@@ -12,11 +12,12 @@ class TimeSeriesModelMultiStepNeuralMLP(TimeSeriesModelMultiStepNeural):
       to find a regression model between the p past values and n future values of the time series.
 
     Parameters:
-      - n_hidden_layers:  number of layers between input & output layer
-      - wd: weight decay >= 0                                           (default: 1e-2)
-      - lr_max_method:  one of ['valley', 'intermediate', 'minimum']    (default: 'minimum')
-      - activation: one of ['elu', 'relu', 'selu']                      (default: 'elu')
-      - n_epochs: >0                                                    (default: 100)
+      - n_hidden_layers:  number of layers between input & output layer             (default: 1)
+      - layer_width: number of neurons in each hidden layer                         (default: 50)
+      - activation: one of ['elu', 'relu', 'selu']                                  (default: 'elu')
+      - wd: weight decay >= 0                                                       (default: 1e-2)
+      - lr_max_method:  one of ['valley', 'intermediate', 'minimum', 'aggressive']  (default: 'minimum')
+      - n_epochs: >0                                                                (default: 100)
     """
 
     # -------------------------------------------------------------------------
@@ -28,7 +29,8 @@ class TimeSeriesModelMultiStepNeuralMLP(TimeSeriesModelMultiStepNeural):
         p: int,
         n: int,
         n_hidden_layers: int = 1,
-        lr_max_method: str = "minimum",  # "valley", "intermediate", "minimum",
+        layer_width: int = 50,
+        lr_max_method: str = "minimum",  # "valley", "intermediate", "minimum", "aggressive"
         n_epochs: int = 100,
         wd: float = 1e-2,
         activation: str = "elu",  # one of "elu", "relu", "selu"
@@ -48,9 +50,11 @@ class TimeSeriesModelMultiStepNeuralMLP(TimeSeriesModelMultiStepNeural):
 
         # init attributes for parameters
         self.n_hidden_layers = 0  # type: int
+        self.layer_width = 0  # type: int
 
         # set parameter values
         self.set_param("n_hidden_layers", n_hidden_layers)
+        self.set_param("layer_width", layer_width)
 
     # -------------------------------------------------------------------------
     #  Model structure
@@ -62,15 +66,12 @@ class TimeSeriesModelMultiStepNeuralMLP(TimeSeriesModelMultiStepNeural):
         Layer sizes are returned as lists of integers.
         """
 
-        # layer width = ceil(sqrt(n*p)), with a minimum of 10
-        layer_width = max(10, math.ceil(math.sqrt(self.n * self.p)))
-
         # each layer same width
-        return [layer_width] * self.n_hidden_layers
+        return [self.layer_width] * self.n_hidden_layers
 
     # -------------------------------------------------------------------------
     #  Model complexity
     # -------------------------------------------------------------------------
     def _model_complexity(self) -> Union[float, tuple]:
         # roughly speaking the number of free parameters in the model
-        return self.p, self.n_hidden_layers, -self.wd
+        return self.p, self.n_hidden_layers, self.layer_width, -self.wd
