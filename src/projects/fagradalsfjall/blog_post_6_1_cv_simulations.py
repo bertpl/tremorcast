f564d253def9f98e6784334bf2be755b3110cba8
Bert Pluymers <bert.pluymers@gmail.com>
2022-05-29 11:50:49 +0200

    Improvements to 1D sweep script.

--- src/projects/fagradalsfjall/blog_post_6_1_cv_simulations.py
+++ src/projects/fagradalsfjall/blog_post_6_1_cv_simulations.py
@@ -1,12 +1,14 @@
+import itertools
 import os
 import pickle
 from enum import Enum, auto
-from typing import Tuple
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from src.base.forecasting.evaluation.cross_validation import ParamSelectionMethod
 from src.base.forecasting.models import TimeSeriesModelMultiStepNeuralMLP, loss_mae
 from src.projects.fagradalsfjall.evaluate_models import get_dataset_train
 from src.tools.matplotlib import plot_style_matplotlib_default
@@ -22,12 +24,14 @@ class Sweep(Enum):
     N_EPOCHS_LR_MAX_VALLEY = auto()
     N_EPOCHS_LR_MAX_INTERMEDIATE = auto()
     N_EPOCHS_LR_MAX_MINIMUM = auto()
+    N_EPOCHS_LR_MAX_AGGRESSIVE = auto()
     N_EPOCHS_WD_LO = auto()
     N_EPOCHS_WD_HI = auto()
     N_EPOCHS_SHALLOW = auto()
     N_EPOCHS_DEEP = auto()
     WD = auto()
     P = auto()
+    LAYER_WIDTH = auto()
     N_LAYERS = auto()
 
     def lower_name(self):
@@ -37,22 +41,10 @@ class Sweep(Enum):
 # =================================================================================================
 #  Main function
 # =================================================================================================
-def blog_6_cv_1d_sweeps(n: int, do_train: bool = True, do_plot: bool = True):
+def blog_6_cv_1d_sweeps(n: int, do_train: bool = True, do_plot: bool = True, sweeps: List[Sweep] = None):
 
     # --- define sweeps -----------------------------------
-
-    sweeps = [
-        Sweep.N_EPOCHS_LR_MAX_VALLEY,
-        Sweep.N_EPOCHS_LR_MAX_INTERMEDIATE,
-        Sweep.N_EPOCHS_LR_MAX_MINIMUM,
-        Sweep.N_EPOCHS_WD_LO,
-        Sweep.N_EPOCHS_WD_HI,
-        Sweep.N_EPOCHS_SHALLOW,
-        Sweep.N_EPOCHS_DEEP,
-        Sweep.WD,
-        Sweep.P,
-        Sweep.N_LAYERS,
-    ]
+    sweeps = sweeps or list(Sweep)
 
     # --- load training data set --------------------------
     df_train = get_dataset_train().to_dataframe()  # type: pd.DataFrame
@@ -69,14 +61,14 @@ def blog_6_cv_1d_sweeps(n: int, do_train: bool = True, do_plot: bool = True):
             print()
 
             # cv settings
-            cv_settings, *_ = _get_cv_settings(sweep, n)
+            cv_settings, *_ = _get_cv_settings_1d(sweep, n)
             cv_model = TimeSeriesModelMultiStepNeuralMLP(signal_name=FORECAST_SIGNAL_NAME, n=0, p=0, cv=cv_settings)
 
             # perform sweep
             cv_model.fit(df_train)
 
             # save model
-            model_filename = get_filename_model(sweep, n)
+            model_filename = get_filename_1d_sweep_model(sweep, n)
             with open(model_filename, "wb") as f:
                 pickle.dump(cv_model, f)
 
@@ -88,7 +80,7 @@ def blog_6_cv_1d_sweeps(n: int, do_train: bool = True, do_plot: bool = True):
 # =================================================================================================
 #  Helpers - CV Settings
 # =================================================================================================
-def _get_cv_settings(sweep: Sweep, n: int) -> Tuple[dict, str, bool, str]:
+def _get_cv_settings_1d(sweep: Sweep, n: int) -> Tuple[dict, str, bool, str]:
     """
     Returns cv_settings dict & related info based on parameter over which we want to do 1D sweep
     :param sweep: (Sweep) which 1D sweep for which to return info
@@ -99,26 +91,24 @@ def _get_cv_settings(sweep: Sweep, n: int) -> Tuple[dict, str, bool, str]:
     # --- nominal settings --------------------------------
 
     # larger n seems to benefit from larger n_epochs without risk of overfitting or instability
-    if n < 24:
-        nominal_n_epochs = 100
-    elif n < 96:
-        nominal_n_epochs = 200
-    else:
-        nominal_n_epochs = 500
+    nominal_lr_max_method = "minimum"
+    nominal_n_epochs = 50
 
     # other settings
     cv_settings = dict(
-        n_splits=10,
-        randomize=True,
+        n_splits=5,
+        randomize=False,
         randomize_runs=True,
         loss=loss_mae,
+        selection_method=ParamSelectionMethod.DEFENSIVE,
         param_grid={
             "n_hidden_layers": [3],
+            "layer_width": [100],
             "n": [n],
-            "p": [96],
+            "p": [16],
             "wd": [0.1],
             "n_epochs": [nominal_n_epochs],
-            "lr_max_method": ["minimum"],
+            "lr_max_method": [nominal_lr_max_method],
         },
     )
 
@@ -128,62 +118,55 @@ def _get_cv_settings(sweep: Sweep, n: int) -> Tuple[dict, str, bool, str]:
 
     if sweep == Sweep.N_EPOCHS_WD_LO:
         cv_settings["param_grid"]["wd"] = [0.0]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "wd=0"
     elif sweep == Sweep.N_EPOCHS_WD_HI:
         cv_settings["param_grid"]["wd"] = [1.0]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "wd=1.0"
     elif sweep == Sweep.N_EPOCHS_LR_MAX_VALLEY:
         cv_settings["param_grid"]["lr_max_method"] = ["valley"]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "lr_max='valley'"
     elif sweep == Sweep.N_EPOCHS_LR_MAX_INTERMEDIATE:
         cv_settings["param_grid"]["lr_max_method"] = ["intermediate"]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "lr_max='intermediate'"
     elif sweep == Sweep.N_EPOCHS_LR_MAX_MINIMUM:
         cv_settings["param_grid"]["lr_max_method"] = ["minimum"]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "lr_max='minimum'"
+    elif sweep == Sweep.N_EPOCHS_LR_MAX_AGGRESSIVE:
+        cv_settings["param_grid"]["lr_max_method"] = ["aggressive"]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
+        param_name = "n_epochs"
+        sub_title = "lr_max='aggressive'"
     elif sweep == Sweep.N_EPOCHS_SHALLOW:
         cv_settings["param_grid"]["n_hidden_layers"] = [1]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "n_hidden_layers=1"
     elif sweep == Sweep.N_EPOCHS_DEEP:
         cv_settings["param_grid"]["n_hidden_layers"] = [10]
-        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        cv_settings["param_grid"]["n_epochs"] = [1, 2, 5, 10, 20, 50, 100, 200, 500]
         param_name = "n_epochs"
         sub_title = "n_hidden_layers=10"
     elif sweep == Sweep.WD:
-        cv_settings["param_grid"]["wd"] = [
-            0.001,
-            0.002,
-            0.005,
-            0.01,
-            0.02,
-            0.05,
-            0.1,
-            0.2,
-            0.5,
-            1,
-            2,
-            5,
-            10,
-            20,
-            50,
-            100,
-        ]
+        cv_settings["param_grid"]["wd"] = sorted(
+            [a * b for a, b in itertools.product([1, 2, 5], [0.001, 0.01, 0.1, 1, 10])]
+        )
         param_name = "wd"
     elif sweep == Sweep.P:
         cv_settings["param_grid"]["p"] = [4, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 288, 384]
         param_name = "p"
+    elif sweep == Sweep.LAYER_WIDTH:
+        cv_settings["param_grid"]["layer_width"] = [10, 20, 50, 75, 100, 150, 200, 500, 1000]
+        param_name = "layer_width"
     elif sweep == Sweep.N_LAYERS:
         cv_settings["param_grid"]["n_hidden_layers"] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         param_name = "n_hidden_layers"
@@ -195,19 +178,45 @@ def _get_cv_settings(sweep: Sweep, n: int) -> Tuple[dict, str, bool, str]:
     return cv_settings, param_name, log_x_scale, sub_title
 
 
+def get_cv_settings_full(n: int) -> dict:
+
+    cv_settings = dict(
+        n_splits=5,
+        randomize=False,
+        randomize_runs=True,
+        loss=loss_mae,
+        selection_method=ParamSelectionMethod.DEFENSIVE,
+        param_grid={
+            "n_hidden_layers": [1, 3],
+            "layer_width": [100],
+            "n": [n],
+            "p": [4, 8, 16, 32],
+            "wd": [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0],
+            "n_epochs": [50, 100, 200],
+            "lr_max_method": ["minimum"],
+        },
+    )
+
+    return cv_settings
+
+
 # =================================================================================================
 #  Helpers - File Names
 # =================================================================================================
-def get_filename_base(sweep: Sweep, n: int) -> str:
+def get_filename_1d_sweep_base(sweep: Sweep, n: int) -> str:
     return os.path.join(_get_output_path("post_6_nn"), f"1d_sweep_{n}_step_{sweep.lower_name()}")
 
 
-def get_filename_fig(sweep: Sweep, n: int) -> str:
-    return get_filename_base(sweep, n) + ".png"
+def get_filename_1d_sweep_fig(sweep: Sweep, n: int) -> str:
+    return get_filename_1d_sweep_base(sweep, n) + ".png"
 
 
-def get_filename_model(sweep: Sweep, n: int) -> str:
-    return get_filename_base(sweep, n) + ".pkl"
+def get_filename_1d_sweep_model(sweep: Sweep, n: int) -> str:
+    return get_filename_1d_sweep_base(sweep, n) + ".pkl"
+
+
+# def get_filename_full_cv(n: int) -> str:
+#     return os.path.join(_get_output_path("post_6_nn"), f"1d_sweep_{n}_step_{sweep.lower_name()}")
 
 
 # =================================================================================================
@@ -219,12 +228,12 @@ def plot_sweep_result(sweep: Sweep, n: int):
     plot_style_matplotlib_default()
 
     # --- load model --------------------------------------
-    model_filename = get_filename_model(sweep, n)
+    model_filename = get_filename_1d_sweep_model(sweep, n)
     with open(model_filename, "rb") as f:
         cv_model = pickle.load(f)  # type: TimeSeriesModelMultiStepNeuralMLP
 
     # --- get info ----------------------------------------
-    _, param_name, log_x_scale, sub_title = _get_cv_settings(sweep, n)
+    _, param_name, log_x_scale, sub_title = _get_cv_settings_1d(sweep, n)
 
     # --- extract results ---------------------------------
     param_values = np.array([cv_result["params"][param_name] for cv_result in cv_model.cv_results["all"]])
@@ -272,8 +281,11 @@ def plot_sweep_result(sweep: Sweep, n: int):
     ax.set_xticklabels([str(pv) for pv in param_values])
 
     ax.set_ylabel("MAE")
-    y_max = max(max(training_losses_mean), max(validation_losses_mean))
-    y_max = min(10 * min(validation_losses_mean), 1.2 * y_max)  # make sure minimum validation loss is still discernible
+
+    y_max_ideal = 2 * np.median(
+        validation_losses_mean + validation_losses_std
+    )  # center median of mean+std validation losses
+    y_max = min(10 * min(validation_losses_mean), y_max_ideal)  # make sure minimum validation loss is still discernible
     ax.set_ylim(bottom=0.0, top=y_max)
 
     fig.suptitle("1D sweep cross-validation results" + (f"\n({sub_title})" if sub_title else ""))
@@ -282,20 +294,28 @@ def plot_sweep_result(sweep: Sweep, n: int):
     fig.tight_layout()
 
     # --- lines & best performance ---
-    min_val_loss = min(validation_losses_mean)
-    param_best = param_values[list(validation_losses_mean).index(min_val_loss)]
+    i_best = list(validation_losses_mean + validation_losses_std).index(
+        min(validation_losses_mean + validation_losses_std)
+    )
+    best_loss_mean_std = validation_losses_mean[i_best] + validation_losses_std[i_best]
+    best_loss_mean = validation_losses_mean[i_best]
+    best_param = param_values[i_best]
 
     x_min, x_max = ax.get_xlim()
+    y_min, y_max = ax.get_ylim()
 
-    # line + text
-    ax.plot([x_min, x_max], [min_val_loss, min_val_loss], "g--", alpha=0.5)
-    ax.text(x_min, 0.95 * min_val_loss, f" {min_val_loss:.3f}", ha="left", va="top", color="g")
+    # line + text + dot - MEAN + STD
+    ax.plot([x_min, x_max], [best_loss_mean_std, best_loss_mean_std], "g--", alpha=0.5)
+    ax.text(x_min, best_loss_mean_std + 0.01 * y_max, f" {best_loss_mean_std:.3f}", ha="left", va="bottom", color="g")
+    ax.plot(best_param, best_loss_mean_std, "go")
 
-    # circle around best result
-    ax.plot(param_best, min_val_loss, "go")
+    # line + text - MEAN
+    ax.plot([x_min, x_max], [best_loss_mean, best_loss_mean], "g--", alpha=0.5)
+    ax.text(x_min, best_loss_mean - 0.01 * y_max, f" {best_loss_mean:.3f}", ha="left", va="top", color="g")
 
+    # reset limits
     ax.set_xlim(x_min, x_max)
 
     # --- save fig ----------------------------------------
-    fig_filename = get_filename_fig(sweep, n)
+    fig_filename = get_filename_1d_sweep_fig(sweep, n)
     fig.savefig(fig_filename, dpi=600)
