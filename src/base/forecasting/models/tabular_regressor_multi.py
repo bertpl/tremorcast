f6ad9cbfd39321bd99efa5d88fab838b1c063d16
Bert Pluymers <bert.pluymers@gmail.com>
2022-06-11 14:55:42 +0200

    Improve processing of cross-validation results.

--- src/base/forecasting/models/tabular_regressor_multi.py
+++ src/base/forecasting/models/tabular_regressor_multi.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from typing import List, Union
+from typing import Any, Dict, List, Union
 
 import numpy as np
 
-from .tabular_regressor import ScoreMetric, TabularRegressor
+from .tabular_regressor import CVResults, ScoreMetric, TabularRegressor
 
 
 # =================================================================================================
@@ -71,10 +71,14 @@ class TabularRegressorMulti(TabularRegressor):
     # -------------------------------------------------------------------------
     #  Hyper-parameters
     # -------------------------------------------------------------------------
-    def set_sub_params(self, **kwargs):
+    def set_sub_params(self, i_sub_models: List[int] = None, **kwargs):
         """Sets the provided keywords arguments as parameters in each sub-model"""
-        for r in self.sub_models:
-            r.set_params(**kwargs)
+        if i_sub_models is None:
+            for m in self.sub_models:
+                m.set_params(**kwargs)
+        else:
+            for i in i_sub_models:
+                self.sub_models[i].set_params(**kwargs)
 
     # -------------------------------------------------------------------------
     #  Hooks
@@ -130,4 +134,55 @@ class SubModelCrossValidation:
             sub_model.cv.grid_search(x, y_sub, param_grid, score_metric, n_splits, shuffle_data, n_jobs)
 
         # --- collect results -----------------------------
-        self.results = {i_sub_model: self.regressor.sub_models[i_sub_model].cv.results for i_sub_model in i_sub_models}
+        self.results = {
+            i_sub_model: self.regressor.sub_models[i_sub_model].cv.results for i_sub_model in i_sub_models
+        }  # type: Dict[int, CVResults]
+
+        # --- show results --------------------------------
+        self.show_results()
+
+    # -------------------------------------------------------------------------
+    #  Show results
+    # -------------------------------------------------------------------------
+    def show_results(self):
+
+        # init
+        all_results = self.get_best_hyper_params()
+        col_names = sorted(list(all_results.values())[0].keys())
+        col_width = 20
+
+        # show
+        print("-" * 120)
+        print("Sub-model grid-search cv results:")
+        print(" " * col_width + "".join([cn.ljust(col_width) for cn in col_names]))
+
+        for i, result in all_results.items():
+            print(
+                f"{i}".ljust(col_width)
+                + "".join(
+                    [
+                        f"{value}".ljust(col_width)
+                        if (len(f"{value}") < 15) or not isinstance(value, float)
+                        else f"{value:.5f}".ljust(col_width)
+                        for value in [result.get(cn, "/") for cn in col_names]
+                    ]
+                )
+            )
+        print("-" * 120)
+
+    def get_best_hyper_params(self) -> Dict[int, Dict[str, Any]]:
+        """Return optimal hyper-parameter values for each submodel, resulting from cross-validation."""
+
+        dct = dict()
+
+        param_names = sorted(list(self.results.values())[0].all_param_values().keys())
+        for i, result in self.results.items():
+
+            best_params = {pn: result.best_result.params.get(pn, "/") for pn in param_names}
+            best_params["loss"] = result.best_result.val_metric_mean
+            if hasattr(self.regressor.sub_models[i], "last_lr_max"):
+                best_params["lr_max_value"] = self.regressor.sub_models[i].last_lr_max
+
+            dct[i] = best_params
+
+        return dct
