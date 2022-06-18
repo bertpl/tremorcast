a4f1ffe5eacd66beb5960e114c2ee9ab849ab168
Bert Pluymers <bert.pluymers@gmail.com>
2022-06-18 15:13:49 +0200

    Implement TabularRegressorOLS.

---
+++ src/base/forecasting/models/tabular/tabular_regressor_ols.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from sklearn.linear_model import LinearRegression
+
+from .feature_selectors import FeatureSelector
+from .tabular_regressor_sklearn import TabularRegressorSklearn
+
+
+class TabularRegressorOLS(TabularRegressorSklearn):
+    def __init__(self, feature_selector: Optional[FeatureSelector] = None):
+
+        super().__init__(
+            name="ols",
+            model=LinearRegression(
+                fit_intercept=False,
+            ),
+            remove_nans_before_fit=True,
+            feature_selector=feature_selector,
+        )
