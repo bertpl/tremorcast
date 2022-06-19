633b637ee5bbd338facd02fe02f351dd94a8006d
Bert Pluymers <bert.pluymers@gmail.com>
2022-06-19 11:03:25 +0200

    Various missing imports.

--- src/base/forecasting/models/tabular/__init__.py
+++ src/base/forecasting/models/tabular/__init__.py
@@ -1,2 +1,8 @@
+from .feature_selectors import FeatureSelector, FeatureSelector_All, FeatureSelector_ExponentialSpacing
+from .mlp_base import MLP, Activation, LrMaxCriterion
 from .tabular_regressor import CVResults, ScoreMetric, TabularRegressor
+from .tabular_regressor_mlp import TabularRegressorMLP
+from .tabular_regressor_mlp_multi import TabularRegressorMLPMulti
+from .tabular_regressor_ols import TabularRegressorOLS
+from .tabular_regressor_pls import TabularRegressorPLS
 from .tabular_regressor_wrapper import ScalingType, TabularRegressorWrapper
