75cefb87c98b989cddcfc1cead1798de40f1b450
Bert Pluymers <bert.pluymers@gmail.com>
2022-06-20 19:06:06 +0200

    Make sure we also determine lr_max separately for each seed.

--- src/base/forecasting/models/tabular/mlp_base.py
+++ src/base/forecasting/models/tabular/mlp_base.py
@@ -77,6 +77,7 @@ class MLP(BaseEstimator, RegressorMixin):
         self.show_progress = show_progress
 
         # other
+        self.seed = None  # type: Optional[int]
         self.last_lr_max_value = None  # type: Optional[float]
 
         # internal
@@ -98,119 +99,19 @@ class MLP(BaseEstimator, RegressorMixin):
         x, y = remove_nan_rows(x, y)
         self._learn_dimensions(x, y)
 
-        # --- determine lr_max ----------------------------
-        lr_max = self._determine_lr_max(x, y)
-
         # --- train ---------------------------------------
-        self._train_best_of_n_seeds(x, y, lr_max)
+        self._fit_best_of_n_seeds(x, y)
 
         # --- return --------------------------------------
         return self
 
-    # -------------------------------------------------------------------------
-    #  Metrics
-    # -------------------------------------------------------------------------
-    def training_losses(self) -> np.ndarray:
-        return np.array(self._nn.recorder.losses)
-
-    # -------------------------------------------------------------------------
-    #  Predict
-    # -------------------------------------------------------------------------
-    def predict(self, x: np.ndarray, **predict_params) -> np.ndarray:
-
-        # construct DataLoader with features
-        #  (see: https://docs.fast.ai/tutorial.tabular.html)
-        df_features = pd.DataFrame(data=x, columns=self._feature_names)
-        dl = self._nn.dls.test_dl(df_features)
-
-        # generate predictions
-        pred, _ = self._nn.get_preds(dl=dl)
-
-        # return as numpy array
-        return np.array(pred)
-
-    # -------------------------------------------------------------------------
-    #  Internal helpers
-    # -------------------------------------------------------------------------
     def _learn_dimensions(self, x: np.ndarray, y: np.ndarray):
         self._n_features = x.shape[1]
         self._n_targets = y.shape[1]
         self._feature_names = [f"x{i}" for i in range(self._n_features)]
         self._target_names = [f"y{i}" for i in range(self._n_targets)]
 
-    def _get_layer_sizes(self) -> List[int]:
-        return [self.layer_width] * self.n_hidden_layers
-
-    def _initialize_nn(self, x: np.ndarray, y: np.ndarray, seed: int):
-
-        # --- set seeds -----------------------------------
-        set_all_random_seeds(seed)
-
-        # --- convert to df -------------------------------
-        df = pd.DataFrame(data=np.concatenate([x, y], axis=1), columns=self._feature_names + self._target_names)
-
-        # --- construct dataloader ------------------------
-        #  (set first sample as validation set, to avoid annoying errors)
-        data_loader = TabularDataLoaders.from_df(
-            df, cont_names=self._feature_names, y_names=self._target_names, valid_idx=[0]
-        )
-
-        # --- tabular model -------------------------------
-        model = TabularModel(
-            emb_szs=[],
-            n_cont=self._n_features,
-            out_sz=self._n_targets,
-            layers=self._get_layer_sizes(),
-            act_cls=self.activation.get_activation(),
-        )
-
-        self._nn = TabularLearner(data_loader, model, metrics=rmse, wd=self.wd)
-
-    def _determine_lr_max(self, x: np.ndarray, y: np.ndarray) -> float:
-
-        # --- determine lr_max ----------------------------
-        if isinstance(self.lr_max, (float, int)):
-            # FIXED lr_max
-            lr_max = float(self.lr_max)
-
-        elif isinstance(self.lr_max, LrMaxCriterion):
-            # use lr_find
-
-            try:
-
-                self._initialize_nn(x, y, seed=1_234)
-                lr_max = self._nn.lr_find(start_lr=1e-6, end_lr=1e3, suggest_funcs=[valley, minimum], show_plot=False)
-
-                if self.lr_max == LrMaxCriterion.VALLEY:
-                    lr_max = lr_max.valley
-                elif self.lr_max == LrMaxCriterion.INTERMEDIATE:
-                    lr_max = math.sqrt(lr_max.valley * lr_max.minimum)
-                elif self.lr_max == LrMaxCriterion.MINIMUM:
-                    lr_max = lr_max.minimum
-                elif self.lr_max == LrMaxCriterion.AGGRESSIVE:
-                    lr_max = 2 * lr_max.minimum
-                else:
-                    raise NotImplementedError(f"lr_max method='{self.lr_max}' not implemented.")
-
-            except Exception as e:
-
-                print(e)
-
-                print("------====== lr_find failed --> falling back to lr_max=1e-3 ======------")
-                lr_max = 1e-3
-
-        else:
-            # not supported
-
-            raise NotImplementedError(f"unsupported type for lr_max parameters: '{type(self.lr_max)}'")
-
-        # --- store & return lr_max -----------------------
-        if self.show_progress:
-            print(f"lr_max: {lr_max:.3e}  [self.lr_max='{self.lr_max}']")
-
-        return lr_max
-
-    def _train_best_of_n_seeds(self, x: np.ndarray, y: np.ndarray, lr_max: float):
+    def _fit_best_of_n_seeds(self, x: np.ndarray, y: np.ndarray):
 
         # --- init ----------------------------------------
         all_solutions = []  # type: List[Tuple[float, float, TabularLearner]]
@@ -219,7 +120,9 @@ class MLP(BaseEstimator, RegressorMixin):
         # --- train for n seeds ---------------------------
         for seed in range(self.n_seeds):
 
-            self._learn_one_cycle_until_convergence(x, y, lr_max, seed)
+            self.seed = seed
+
+            self._fit_one_cycle_until_convergence(x, y)
             final_rmse = self.training_losses()[-1]
 
             all_solutions.append((final_rmse, self.last_lr_max_value, self._nn))
@@ -246,28 +149,134 @@ class MLP(BaseEstimator, RegressorMixin):
 
         self._nn = best_nn
 
-    def _learn_one_cycle_until_convergence(self, x: np.ndarray, y: np.ndarray, lr_max: float, seed: int):
+    def _fit_one_cycle_until_convergence(self, x: np.ndarray, y: np.ndarray):
         """Runs _learn_one_cycle with decreasing values of lr_max until we converge."""
 
+        # --- determine lr_max ----------------------------
+        lr_max = self._determine_lr_max(x, y)
+
+        # --- fit until convergence -----------------------
         self.last_lr_max_value = None
         while (self.last_lr_max_value is None) or self._learner_weights_are_nan():
 
-            self._initialize_nn(x, y, seed)
-            self._learn_one_cycle(lr_max, seed)
+            self._fit_one_cycle(x, y, lr_max)
 
             if self._learner_weights_are_nan():
-                print(f"WARNING: training was unstable; reducing lr_max from {lr_max} to {lr_max / 10}.")
                 lr_max = lr_max / 10
+                if self.show_progress:
+                    print(f"WARNING: training was unstable; reducing lr_max to {lr_max}.")
 
-    def _learn_one_cycle(self, lr_max: float, seed: int):
+    def _determine_lr_max(self, x: np.ndarray, y: np.ndarray) -> float:
 
-        extra_msg = f"seed={seed}".ljust(8) if self.n_seeds > 1 else ""
+        # --- initialize nn -------------------------------
+        self._initialize_nn(x, y)
+
+        # --- determine lr_max ----------------------------
+        if isinstance(self.lr_max, (float, int)):
+            # FIXED lr_max
+            lr_max = float(self.lr_max)
+
+        elif isinstance(self.lr_max, LrMaxCriterion):
+            # use lr_find
+
+            try:
+
+                lr_max = self._nn.lr_find(start_lr=1e-6, end_lr=1e3, suggest_funcs=[valley, minimum], show_plot=False)
+
+                if self.lr_max == LrMaxCriterion.VALLEY:
+                    lr_max = lr_max.valley
+                elif self.lr_max == LrMaxCriterion.INTERMEDIATE:
+                    lr_max = math.sqrt(lr_max.valley * lr_max.minimum)
+                elif self.lr_max == LrMaxCriterion.MINIMUM:
+                    lr_max = lr_max.minimum
+                elif self.lr_max == LrMaxCriterion.AGGRESSIVE:
+                    lr_max = 2 * lr_max.minimum
+                else:
+                    raise NotImplementedError(f"lr_max method='{self.lr_max}' not implemented.")
+
+            except Exception as e:
+
+                print(e)
+
+                print("------====== lr_find failed --> falling back to lr_max=1e-3 ======------")
+                lr_max = 1e-3
+
+        else:
+            # not supported
+
+            raise NotImplementedError(f"unsupported type for lr_max parameters: '{type(self.lr_max)}'")
+
+        # --- store & return lr_max -----------------------
+        return lr_max
+
+    def _initialize_nn(self, x: np.ndarray, y: np.ndarray):
+
+        # --- set all seeds -------------------------------
+        set_all_random_seeds(self.seed)
+
+        # --- convert to df -------------------------------
+        df = pd.DataFrame(data=np.concatenate([x, y], axis=1), columns=self._feature_names + self._target_names)
+
+        # --- construct dataloader ------------------------
+        #  (set first sample as validation set, to avoid annoying errors)
+        data_loader = TabularDataLoaders.from_df(
+            df, cont_names=self._feature_names, y_names=self._target_names, valid_idx=[0]
+        )
+
+        # --- tabular model -------------------------------
+        model = TabularModel(
+            emb_szs=[],
+            n_cont=self._n_features,
+            out_sz=self._n_targets,
+            layers=[self.layer_width] * self.n_hidden_layers,
+            act_cls=self.activation.get_activation(),
+        )
+
+        self._nn = TabularLearner(data_loader, model, metrics=rmse, wd=self.wd)
+
+    def _fit_one_cycle(self, x: np.ndarray, y: np.ndarray, lr_max: float):
+
+        # --- initialize nn -------------------------------
+        self._initialize_nn(x, y)
+
+        # --- determine progress msg ----------------------
+        extra_msg = f"seed={self.seed}".ljust(8) if self.n_seeds > 1 else ""
+        extra_msg = extra_msg + f"| lr_max={lr_max:.2e}"
+
+        # --- actual fitting ------------------------------
         add_tqdm_callback(self._nn, enabled=self.show_progress, extra_msg=extra_msg)
         self._nn.fit_one_cycle(self.n_epochs, lr_max=lr_max)
         remove_tqdm_callback(self._nn)
 
+        # --- remember lr_max -----------------------------
         self.last_lr_max_value = lr_max
 
     def _learner_weights_are_nan(self) -> bool:
         all_params = [p.T.detach().numpy() for p in self._nn.parameters()]
         return any([any(np.isnan(arr.flatten())) or any(np.isinf(arr.flatten())) for arr in all_params])
+
+    # -------------------------------------------------------------------------
+    #  Metrics
+    # -------------------------------------------------------------------------
+    def training_losses(self) -> np.ndarray:
+        return np.array(self._nn.recorder.losses)
+
+    # -------------------------------------------------------------------------
+    #  Predict
+    # -------------------------------------------------------------------------
+    def predict(self, x: np.ndarray, **predict_params) -> np.ndarray:
+
+        # construct DataLoader with features
+        #  (see: https://docs.fast.ai/tutorial.tabular.html)
+        df_features = pd.DataFrame(data=x, columns=self._feature_names)
+        dl = self._nn.dls.test_dl(df_features)
+
+        # generate predictions
+        pred, _ = self._nn.get_preds(dl=dl)
+
+        # return as numpy array
+        return np.array(pred)
+
+    # -------------------------------------------------------------------------
+    #  Internal helpers
+    # -------------------------------------------------------------------------
