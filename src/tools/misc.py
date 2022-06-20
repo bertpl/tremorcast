6e89d9dcba88ff5a9f6166ecfda4a06d8262b9c2
Bert Pluymers <bert.pluymers@gmail.com>
2022-06-20 19:05:08 +0200

    Formatting.

--- src/tools/misc.py
+++ src/tools/misc.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Iterable
+from typing import Any, Iterable, List
 
 
 # =================================================================================================
