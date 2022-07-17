23317f97ea421c9c8c95e9d59ea02e2fd6feb7a2
Bert Pluymers <bert.pluymers@gmail.com>
2022-07-17 12:21:39 +0200

    Generalize materialize_param_grid to have a list-encapsulation-switch + update unit tests.

--- src/base/forecasting/evaluation/cross_validation/param_grids.py
+++ src/base/forecasting/evaluation/cross_validation/param_grids.py
@@ -23,7 +23,10 @@ class CVMetaData:
 #  Parameter grid handling
 # =================================================================================================
 def materialize_param_grid(
-    param_grid: Union[Dict, List[Dict]], shuffle: bool = True, add_meta_info: bool = True
+    param_grid: Union[Dict, List[Dict]],
+    shuffle: bool = True,
+    add_meta_info: bool = True,
+    encapsulate_param_values_in_list: bool = True,
 ) -> List[Dict]:
     """Materializes a GridSearchCV param_grid into a list of single-param-set dicts with cv-meta-info attached"""
 
@@ -55,7 +58,7 @@ def materialize_param_grid(
     # --- materialize ---------------------------------
     param_set_list = [
         {
-            param: [value]
+            param: value
             for param_tuple, param_value_tuple in param_set.items()
             for param, value in zip(param_tuple, param_value_tuple)
         }
@@ -77,7 +80,14 @@ def materialize_param_grid(
         now = datetime.datetime.now()
         n_param_sets = len(param_set_list)
         for i_param_set, param_set in enumerate(param_set_list):
-            param_set[CV_METADATA_PARAM] = [CVMetaData(now, i_param_set, n_param_sets)]
+            param_set[CV_METADATA_PARAM] = CVMetaData(now, i_param_set, n_param_sets)
+
+    # --- optionally encapsulate in lists -------------
+    if encapsulate_param_values_in_list:
+        # sklearn's GridSearchCV expects it like this.  Our own internal grid search cv implementations do not.
+        param_set_list = [
+            {param_name: [param_value] for param_name, param_value in param_set.items()} for param_set in param_set_list
+        ]
 
     # --- return --------------------------------------
     return param_set_list
--- src/base/forecasting/evaluation/cross_validation/tests/test_param_grids.py
+++ src/base/forecasting/evaluation/cross_validation/tests/test_param_grids.py
@@ -7,18 +7,32 @@ from src.tools.misc import sort_any
 
 
 @pytest.mark.parametrize(
-    "param_grid, expected_result",
+    "param_grid, encapsulate_in_list, expected_result",
     [
-        # regular case
+        # regular case - with encapsulation
         (
             # input:
             {"a": [1, 10], "b": [2, 3]},
+            True,
             # expected output:
             [
                 {"a": [1], "b": [2]},
                 {"a": [1], "b": [3]},
                 {"a": [10], "b": [2]},
                 {"a": [10], "b": [3]},
+            ],
+        ),
+        # regular case - without encapsulation
+        (
+            # input:
+            {"a": [1, 10], "b": [2, 3]},
+            False,
+            # expected output:
+            [
+                {"a": 1, "b": 2},
+                {"a": 1, "b": 3},
+                {"a": 10, "b": 2},
+                {"a": 10, "b": 3},
             ],
         ),
         # list of dicts
@@ -28,6 +42,7 @@ from src.tools.misc import sort_any
                 {"a": [1], "b": [2, 3]},
                 {"a": [10], "b": [3, 5]},
             ],
+            True,
             # expected output:
             [
                 {"a": [1], "b": [2]},
@@ -43,6 +58,7 @@ from src.tools.misc import sort_any
                 {("a", "b"): [(1, 2), (1, 3)]},
                 {("a", "b"): [(10, 3), (10, 5)]},
             ],
+            True,
             # expected output:
             [
                 {"a": [1], "b": [2]},
@@ -53,10 +69,14 @@ from src.tools.misc import sort_any
         ),
     ],
 )
-def test_materialize_param_grid(param_grid: Union[dict, List[dict]], expected_result: List[dict]):
+def test_materialize_param_grid(
+    param_grid: Union[dict, List[dict]], encapsulate_in_list: bool, expected_result: List[dict]
+):
 
     # --- act ---------------------------------------------
-    materialized_grid = materialize_param_grid(param_grid, add_meta_info=False)
+    materialized_grid = materialize_param_grid(
+        param_grid, add_meta_info=False, encapsulate_param_values_in_list=encapsulate_in_list
+    )
 
     # --- assert ------------------------------------------
     assert sort_any(materialized_grid) == sort_any(expected_result)
