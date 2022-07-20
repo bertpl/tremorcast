f7eab2e1ac6af825e74daca5b7c7badc20223260
Bert Pluymers <bert.pluymers@gmail.com>
2022-07-20 09:01:31 +0200

    Post 3 - refactor dataset splitting in Jupyter notebook.

--- src/projects/fagradalsfjall/_project_settings.py
+++ src/projects/fagradalsfjall/_project_settings.py
@@ -1,10 +1,11 @@
 import os
+import datetime
 
 from src.tools.paths import get_git_root
 
-# -------------------------------------------------------------------------
-#  Folders
-# -------------------------------------------------------------------------
+# =================================================================================================
+#  Core folder structure
+# =================================================================================================
 PATH_PROJECT_ROOT = os.path.join(get_git_root(), "_data/projects/fagradalsfjall")
 
 PATH_SCRAPED = os.path.join(PATH_PROJECT_ROOT, "0_scraped")
@@ -13,8 +14,12 @@ PATH_DATASET = os.path.join(PATH_PROJECT_ROOT, "1_dataset")
 PATH_BLOG_POSTS = os.path.join(PATH_PROJECT_ROOT, "2_blog_posts")
 
 
+# =================================================================================================
+#  BLOG 2 - Construct dataset
+# =================================================================================================
+
 # -------------------------------------------------------------------------
-#  SOURCE FILES
+#  INPUT - scraped files
 # -------------------------------------------------------------------------
 SCRAPED_FILES_FOR_PROCESSING = [
     os.path.join(PATH_SCRAPED, filename)
@@ -36,16 +41,41 @@ SCRAPED_FILES_FOR_PROCESSING = [
 ]
 
 # -------------------------------------------------------------------------
-#  DATASET
+#  OUTPUT - full dataset
 # -------------------------------------------------------------------------
-
-# STEP 0 - extract data from plots
 FILE_DATASET_FULL = os.path.join(PATH_DATASET, "dataset_full")
 
-# STEP 1 - prepare train & test set
+# =================================================================================================
+#  BLOG 3 - Train & test split + naive models
+# =================================================================================================
+
+# -------------------------------------------------------------------------
+#  DATASET - train / test split
+# -------------------------------------------------------------------------
+
+# --- paths -----------------------------------------------
 FILE_DATASET_SELECTION = os.path.join(PATH_DATASET, "dataset_selection")
 FILE_DATASET_TRAIN = os.path.join(PATH_DATASET, "dataset_train")
 FILE_DATASET_TEST = os.path.join(PATH_DATASET, "dataset_test")
 
+# --- settings --------------------------------------------
+# TOTAL NUMBER OF DAYS = 45
+DATASET_TRAIN_TEST_TS_FROM = datetime.datetime(2021, 7, 20, 0, 0)   # start of long, clean stretch of data
+DATASET_TRAIN_TEST_TS_TO = datetime.datetime(2021, 9, 3, 0, 0)   # end of long, clean stretch of data
+
+# train & test sample ranges
+# TRAIN: first 31 days (1 month)
+DATASET_TRAIN_SAMPLE_FROM = 0
+DATASET_TRAIN_SAMPLE_TO = 31 * 96
+
+# TEST: last 14 days
+DATASET_TEST_SAMPLE_FROM = 31 * 96
+DATASET_TEST_SAMPLE_TO = 45 * 96
+
+# -------------------------------------------------------------------------
+#  FORECASTING PROBLEM DEFINITION
+# -------------------------------------------------------------------------
+
+# --- paths -----------------------------------------------
 FORECAST_SIGNAL_NAME = "low_mid"
 FORECAST_MAE_THRESHOLD = 200
